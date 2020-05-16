ca13b97151f606bdc15f45d3484e8d24471cc37a
mibes <mibes@mibesco.com>
2020-05-16 09:17:20 +0200

    Changed docs to be on the crate level.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! ```rust
 //! use randir::utils::generate_entries;
 //!
-//! //! generate 100 random names and telephone numbers
+//! /// generate 100 random names and telephone numbers
 //! let directory = generate_entries(100);
 //! for entry in directory {
 //!     println!("{}", entry)
